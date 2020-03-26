65174b5f1a9669dfb93680c2f9d968c340a08815
J. Christian Romero <jcromerohdz@gmail.com>
2020-03-26 14:49:39 -0700

    Adding sass to the project for homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,42 +1,38 @@
 import React from 'react'
 
+import './homepage.styles.scss';
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>JACKETS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>SHOES</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>WOMENS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MAN</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>MENS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
     </div>
